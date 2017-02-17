a6b35a5faf279a2148dd39d56133bedbc58a01e6
Daniel Zheng <daniel.zheng@cern.ch>
2017-02-17 10:27:18 -0500

    added term to tests

--- tests/textbook_test.py
+++ tests/textbook_test.py
@@ -11,9 +11,21 @@ class PittServerDownException(Exception):
 class TextbookTest(unittest.TestCase):
     @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
     def test_textbook_get_books_data(self):
-        ans = textbook.get_books_data([{'department_code': 'CHEM', 'course_name': 'CHEM0120', 'instructor': 'FORTNEY'}, {'department_code': 'CS', 'course_name': 'CS0445', 'instructor': 'GARRISON III'}])
+        ans = textbook.get_books_data([{'department_code': 'CHEM', 'course_name': 'CHEM0120', 'instructor': 'FORTNEY', 'term': '2600'}, {'department_code': 'CS', 'course_name': 'CS0445', 'instructor': 'GARRISON III','term': '2600'}])
         self.assertIsInstance(ans, list)
         self.assertTrue(len(ans) == 6)
-        ans = textbook.get_books_data([{'department_code': 'MATH', 'course_name': 'MATH0220', 'instructor': 'HOCKENSMITH'}])
+        ans = textbook.get_books_data([{'department_code': 'MATH', 'course_name': 'MATH0220', 'instructor': 'HOCKENSMITH', 'term': '2600'}])
         self.assertIsInstance(ans, list)
         self.assertTrue(len(ans) == 2)
+
+    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    def test_invalid_department_code(self):
+        self.assertRaises(ValueError, textbook.get_books_data([{'department_code': 'DOES', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}]))
+
+    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    def test_invalid_course_name(self):
+        self.assertRaises(ValueError, textbook.get_books_data([{'department_code': 'CS', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}]))
+
+    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    def test_invalid_instructor(self):
+        self.assertRaises(ValueError, textbook.get_books_data([{'department_code': 'CS', 'course_name': 'CS0447', 'instructor': 'EXIST', 'term': '2600'}]))
