e55360b8f0ffe2110f4cc22f4f4fe30a472cb4a0
Daniel Zheng <dzheng256@gmail.com>
2017-02-19 12:42:59 -0500

    changed test timeout to default

--- tests/textbook_test.py
+++ tests/textbook_test.py
@@ -2,6 +2,8 @@ import unittest
 import timeout_decorator
 
 from PittAPI import textbook
+from . import PittServerError, DEFAULT_TIMEOUT
+
 
 
 class PittServerDownException(Exception):
@@ -9,21 +11,21 @@ class PittServerDownException(Exception):
 
 
 class TextbookTest(unittest.TestCase):
-    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_textbook_get_books_data(self):
         ans = textbook.get_books_data([
         {'department_code': 'CHEM', 'course_name': 'CHEM0120', 'instructor': 'FORTNEY', 'term': '2600'},
         {'department_code': 'CS', 'course_name': 'CS0445', 'instructor': 'GARRISON III','term': '2600'}])
         self.assertIsInstance(ans, list)
         self.assertTrue(len(ans) == 6)
-
-    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+        
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_textbook_get_books_data_past_22462(self):
         ans = textbook.get_books_data([{'department_code': 'MATH', 'course_name': 'MATH0220', 'instructor': 'HOCKENSMITH', 'term': '2600'}])
         self.assertIsInstance(ans, list)
         self.assertTrue(len(ans) == 2)
 
-    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_textbook_get_books_data_many(self):
         ans = textbook.get_books_data([
         {'department_code': 'MATH', 'course_name': 'MATH0220', 'instructor': 'HOCKENSMITH', 'term': '2600'},
@@ -34,17 +36,16 @@ class TextbookTest(unittest.TestCase):
         self.assertIsInstance(ans, list)
         self.assertTrue(len(ans) == 9)
 
-    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_department_code(self):
         self.assertRaises(ValueError,textbook.get_books_data, [{'department_code': 'DOES', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}])
 
-    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_course_name(self):
         ans = textbook.get_books_data([{'department_code': 'CS', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}])
         self.assertTrue(len(ans) == 0)
 
-    @timeout_decorator.timeout(30, timeout_exception=PittServerDownException)
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_instructor(self):
         ans = textbook.get_books_data([{'department_code': 'CS', 'course_name': 'CS0447', 'instructor': 'EXIST', 'term': '2600'}])
         self.assertTrue(len(ans) == 0)
-        
