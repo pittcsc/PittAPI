b2439f3bd6b59c853c316230f25440f9b1d21d7f
Daniel Zheng <dzheng256@gmail.com>
2017-02-24 11:44:06 -0500

    changed tests to account to account for newly raised ValueError

--- tests/textbook_test.py
+++ tests/textbook_test.py
@@ -18,7 +18,7 @@ class TextbookTest(unittest.TestCase):
         {'department_code': 'CS', 'course_name': 'CS0445', 'instructor': 'GARRISON III','term': '2600'}])
         self.assertIsInstance(ans, list)
         self.assertTrue(len(ans) == 6)
-        
+
     @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_textbook_get_books_data_past_22462(self):
         ans = textbook.get_books_data([{'department_code': 'MATH', 'course_name': 'MATH0220', 'instructor': 'HOCKENSMITH', 'term': '2600'}])
@@ -38,14 +38,12 @@ class TextbookTest(unittest.TestCase):
 
     @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_department_code(self):
-        self.assertRaises(ValueError,textbook.get_books_data, [{'department_code': 'DOES', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}])
+        self.assertRaises(ValueError, textbook.get_books_data, [{'department_code': 'DOES', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}])
 
     @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_course_name(self):
-        ans = textbook.get_books_data([{'department_code': 'CS', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}])
-        self.assertTrue(len(ans) == 0)
+        self.assertRaises(ValueError, textbook.get_books_data, [{'department_code': 'CS', 'course_name': 'NOT', 'instructor': 'EXIST', 'term': '2600'}])
 
     @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_instructor(self):
-        ans = textbook.get_books_data([{'department_code': 'CS', 'course_name': 'CS0447', 'instructor': 'EXIST', 'term': '2600'}])
-        self.assertTrue(len(ans) == 0)
+        self.assertRaises(ValueError, textbook.get_books_data, [{'department_code': 'CS', 'course_name': 'CS0447', 'instructor': 'EXIST', 'term': '2600'}])
