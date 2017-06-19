b6fcf2932fe4e8e644cc5a5237e1b7f7edc706a3
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2017-06-19 08:36:23 -0400

    Added future feature and improved readability

--- PittAPI/textbook.py
+++ PittAPI/textbook.py
@@ -23,6 +23,9 @@ import grequests
 import requests
 from bs4 import BeautifulSoup
 
+# Future change to be implemented
+from . import departments
+
 session = requests.session()
 
 # TODO(Alex Z.): Look into instructor names and whether they are useful
@@ -38,7 +41,10 @@ def _fetch_term_codes():
         return []
     script = BeautifulSoup(page.text, 'lxml').findAll('script')[-2].text
     data = json.loads(script[script.find('['):script.find(']') + 1])
-    terms = [item['id'] for item in data]
+    terms = [
+        item['id']
+        for item in data
+    ]
     return terms
 
 
@@ -55,6 +61,10 @@ def _validate_term(term):
     raise ValueError("Invalid term")
 
 
+def connection_exception(request, exception):
+    pass
+
+
 def get_books_data(courses_info):
     """Returns list of dictionaries of book information."""
     request_objs = []
@@ -62,17 +72,23 @@ def get_books_data(courses_info):
     instructors = []  # need to save these
 
     # TODO(Alex Z.): Validation that argument is a list, add warning and correction is not or raise exception
-    for course in courses_info:
-        book_info = course
+    for book_info in courses_info:
         # TODO(Alex Z.): Check what information is actually needed
+
         course_names.append(book_info['course_name'])
         instructors.append(book_info['instructor'])
-        request_objs.append(
-            grequests.get(_get_department_url(book_info['department_code'], book_info['term']), timeout=10)
-        )
-    responses = grequests.map(request_objs)  # parallel requests
 
-    course_ids = _extract_course_ids(responses, course_names, instructors)
+        request_objs.append(
+            grequests.get(
+                _get_department_url(book_info['department_code'], book_info['term']),
+                timeout=10
+            )
+        )
+
+    responses = grequests.map(request_objs ,exception_handler=connection_exception)  # parallel requests
+    bundle = list(zip(responses, course_names, instructors))
+
+    course_ids = _extract_course_ids(bundle)
     book_data = session.get(_construct_url(course_ids)).text
 
     return _extract_books(book_data)  # return list of dicts of books
@@ -82,14 +98,14 @@ def _construct_url(ids):
     return BASE_URL + 'comparison?id=' + '%2C'.join(ids)
 
 
-def _extract_course_ids(responses, course_names, instructors):
+def _extract_course_ids(bundle):
     ids = []
 
-    for resp, course, instruct in zip(responses, course_names, instructors):
+    for response, course, instruct in bundle:
         sections = []
         course_id = ''
 
-        for course_dict in resp.json():
+        for course_dict in response.json():
             if course_dict['id'] == course:
                 sections = course_dict['sections']
                 break
@@ -104,34 +120,30 @@ def _extract_course_ids(responses, course_names, instructors):
 
 
 def _extract_books(data):
-    books, keys = [], ['isbn', 'citation', 'title', 'edition', 'author']
+    keys = ['isbn', 'citation', 'title', 'edition', 'author']
     # TODO(Alex Z.): Added check for invalid response that return an empty json list
-
-
     start, end = data.find('Verba.Compare.Collections.Sections') + 35, data.find('}]}]);') + 4
     info = [json.loads(data[start:end])][0]  # TODO(Alex Z.) Look into whether it's needed to choose the first index
-
-    for data in info:
-        for book in data['books']:
-            books.append(
-                _filter_dictionary(book, keys)
-            )
-
+    books = [
+        _filter_dictionary(book, keys)
+        for data in info
+        for book in data['books']
+    ]
     return books
 
 
 def _filter_dictionary(d, keys):
-    return dict((k, d[k]) for k in keys if k in d)
+    return dict(
+        (k, d[k])
+        for k in keys
+        if k in d
+    )
 
 
 def _get_department_url(department_code, term):
     """Returns url for given department code."""
-    # TODO(Alex Z.): Fix statements below to something more concrete then using static list of codes
-    department_code = 0
-    # department_number = CODES.index(department_code) + 22399
-    # if department_number > 22462:
-    #     department_number += 2  # between codes DSANE and EAS 2 id numbers are skipped.
-    # if department_number > 22580:
-    #     department_number += 1  # between codes PUBSRV and REHSCI 1 id number is skipped.
-    query = 'compare/courses/?id={}&term_id={}'.format(department_number, _validate_term(term))
+    query = 'compare/courses/?id={}&term_id={}'.format(
+        departments[department_code]['textbook'],
+        _validate_term(term)
+    )
     return BASE_URL + query
