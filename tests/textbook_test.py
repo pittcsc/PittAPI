d99bafae3dfeafc20b12accf3278ec9e091ba0a2
Alex <alex.zharichenko@quantumcode.io>
2017-09-12 08:41:21 -0400

    Removed timeout and custom exception and added another test

--- tests/textbook_test.py
+++ tests/textbook_test.py
@@ -2,10 +2,8 @@ import responses
 import json
 import os
 import unittest
-import timeout_decorator
 
 from PittAPI import textbook
-from . import PittServerError, DEFAULT_TIMEOUT
 
 TERM = '1000'
 SCRIPT_PATH = os.path.dirname(__file__)
@@ -117,11 +115,11 @@ class TextbookTest(unittest.TestCase):
                       json='[{"id":"2096322_9780133744057_0","isbn":"9780133744057","pf_id":null,"new_item_id":"1440565:N","used_item_id":null,"required":"Required","sort_order":["0","1","Data Struct.+Abstract.W/Java-W/Access"],"cover_image_url":"//coverimages.verbacompete.com/09aa6a70-fd2b-5a75-9d41-63a28c87de1a.jpg","title":"Data Struct.+Abstract.W/Java-W/Access","author":"Carrano","notes":null,"citation":"\u003Cem\u003EData Struct.+Abstract.W/Java-W/Access\u003C/em\u003E by Carrano. Pearson Education, 4th Edition, 2014. (ISBN: 9780133744057).","metadata":{"section_id":"2096322","section_code":"22768","term_name":"Fall 17"},"offers":[{"isbn":"9780133744057","retailer":"bookstore","item_id":"1440565:N","condition":"new","title":null,"currency":null,"merchant":"bookstore","rental_days":null,"retailer_name":"University Store","metadata":null,"in_cart":null,"selected":null,"total":"176.2","retailer_order":0,"comments":"Pick it up or ship it!","special_comment":"Pick it up or ship it!","store_branded":true,"data_source":"bookstore","description":"From University Store on Fifth","seller_rating":null,"fcondition":"New","fprice":"$176.20","discounted_shipping":null,"can_checkout":true,"feedback_count":null,"location":null,"price":"176.2","shipping":0,"variant":"new","id":"bookstore_9780133744057_new_1440565:N"},{"isbn":"9780133744057","retailer":"bookstore","item_id":"1440565:NR","condition":"new_rental","title":null,"currency":null,"merchant":"bookstore","rental_days":null,"retailer_name":"University Store","metadata":null,"in_cart":null,"selected":null,"total":"116.4","retailer_order":0,"comments":"Pick it up or ship it!","special_comment":"Pick it up or ship it!","store_branded":true,"data_source":"bookstore","description":"From University Store on Fifth","seller_rating":null,"fcondition":"New Rental","fprice":"$116.40","discounted_shipping":null,"can_checkout":true,"feedback_count":null,"location":null,"price":"116.4","shipping":0,"variant":"rental","id":"bookstore_9780133744057_new_rental_1440565:NR"},{"isbn":"9780133744057","retailer":"bookstore","item_id":"1440565:UR","condition":"used_rental","title":null,"currency":null,"merchant":"bookstore","rental_days":null,"retailer_name":"University Store","metadata":null,"in_cart":null,"selected":null,"total":"72.35","retailer_order":0,"comments":"Books Arriving Every Day-Check Frequently!","special_comment":"Pick it up or ship it!","store_branded":true,"data_source":"bookstore","description":"From University Store on Fifth","seller_rating":null,"fcondition":"Used Rental","fprice":"$72.35","discounted_shipping":null,"can_checkout":false,"feedback_count":null,"location":null,"price":"72.35","shipping":0,"variant":"rental","id":"bookstore_9780133744057_used_rental_1440565:UR"}],"section_id":null,"db_section_id":null,"inclusive_access":false,"catalog_id":23376,"edition":"4","copyright_year":"2015"}]',
                       status=200)
 
-
-@unittest.skip
-class TextbookAPITest(unittest.TestCase):
-    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
+    @responses.activate
     def test_textbook_get_textbook(self):
+        with open(os.path.join(SCRIPT_PATH, 'samples/textbook_courses.json')) as f:
+            responses.add(responses.GET, 'http://pitt.verbacompare.com/compare/courses/?id=22457&term_id=1000',
+                          json=json.load(f), status=201)
         instructor_test = textbook.get_textbook(
             term=TERM,
             department='CS',
@@ -133,12 +131,12 @@ class TextbookAPITest(unittest.TestCase):
             term=TERM,
             department='CS',
             course='445',
-            section='1010'
+            section='1030'
         )
         self.assertIsInstance(instructor_test, list)
         self.assertIsInstance(section_test, list)
 
-    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
+    @unittest.skip
     def test_textbook_get_textbooks(self):
         multi_book_test = textbook.get_textbooks(
             term=TERM,
@@ -147,7 +145,5 @@ class TextbookAPITest(unittest.TestCase):
                 {'department': 'STAT', 'course': '1000', 'instructor': 'YANG'}])
         self.assertIsInstance(multi_book_test, list)
 
-    @unittest.skip
-    @timeout_decorator.timeout(DEFAULT_TIMEOUT, timeout_exception=PittServerError)
     def test_invalid_department_code(self):
         self.assertRaises(ValueError, textbook.get_textbook, TERM, 'TEST', '000', 'EXIST', None)
