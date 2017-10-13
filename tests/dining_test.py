dd0e6e11dfbf224b56f182978a4f288064d48c50
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2017-10-13 00:50:20 -0400

    Unsilenced dining tests

--- tests/dining_test.py
+++ tests/dining_test.py
@@ -21,7 +21,6 @@ import unittest
 
 from PittAPI import dining
 
-@unittest.skip
 class DiningTest(unittest.TestCase):
     def test_get_dining_locations(self):
         self.assertIsInstance(dining.get_locations(), list)
