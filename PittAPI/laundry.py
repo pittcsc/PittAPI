cbdd04efed98bdab4809c0846932b29fb22fef52
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2017-11-04 13:15:27 -0400

    Refactored Laundry API

--- PittAPI/laundry.py
+++ PittAPI/laundry.py
@@ -18,14 +18,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 """
 
 import requests
-from bs4 import BeautifulSoup
 import re
+from typing import Any, Dict, List, Union
 
-from typing import Dict, List, Union
+from bs4 import BeautifulSoup
 
-session = requests.session()
-
-location_lookup = {
+BASE_URL = 'http://m.laundryview.com/submitFunctions.php?monitor=true&lr={}'
+LOCATION_LOOKUP = {
     'TOWERS': '2430136',
     'BRACKENRIDGE': '2430119',
     'HOLLAND': '2430137',
@@ -35,6 +34,15 @@ location_lookup = {
     'SUTH_WEST': '2430134',
     'FORBES_CRAIG': '2430142'
 }
+
+
+def _get_laundry_soup(building_name: str) -> Any:
+    """Returns BeautifulSoup object of laundry view webpage"""
+    building_name = building_name.upper()
+    url = BASE_URL.format(LOCATION_LOOKUP[building_name])
+    response = requests.get(url)
+    laundry_soup = BeautifulSoup(response.text, 'lxml')
+    return laundry_soup
 
 
 def get_status_simple(building_name: str) -> Dict[str, str]:
@@ -51,12 +59,7 @@ def get_status_simple(building_name: str) -> Dict[str, str]:
         -> SUTH_EAST
         -> SUTH_WEST
     """
-
-    building_name = building_name.upper()
-    url = 'http://m.laundryview.com/submitFunctions.php?monitor=true&lr={}'\
-        .format(location_lookup[building_name])
-    response = requests.get(url)
-    laundry_soup = BeautifulSoup(response.text, 'lxml')
+    laundry_soup = _get_laundry_soup(building_name)
     re_format = re.compile(r'^([0-9]+) of ([0-9]+) available$')
     washer_text = laundry_soup.find('span', {'id': 'washer_available'}).text
     dryer_text = laundry_soup.find('span', {'id': 'dryer_available'}).text
@@ -72,16 +75,10 @@ def get_status_simple(building_name: str) -> Dict[str, str]:
     }
 
 
-def get_status_detailed(building_name: str, machine=None) -> List[Dict[str, Union[str, int]]]:
-    building_name = building_name.upper()
+def get_status_detailed(building_name: str) -> List[Dict[str, Union[str, int]]]:
     """
     :returns: A list of washers and dryers for the passed
               building location with their statuses
-
-              OR
-
-              A dict of machine metadata if machine name
-              is explicity passed
 
     :param building_name: (String) one of these:
         -> BRACKENRIDGE
@@ -90,42 +87,27 @@ def get_status_detailed(building_name: str, machine=None) -> List[Dict[str, Unio
         -> MCCORMICK
         -> SUTH_EAST
         -> SUTH_WEST
-    :param machine: (String) specific machine to query
-                    eg: machine='BA02'
     """
-    building_name = building_name.upper()
     machines = []
-
-    url = 'http://m.laundryview.com/submitFunctions.php?monitor=true&lr={}' \
-        .format(location_lookup[building_name])
-    response = requests.get(url)
-    laundry_soup = BeautifulSoup(response.text, 'lxml')
-    is_washer = False
+    machine_type = "Unknown"
+    laundry_soup = _get_laundry_soup(building_name)
 
     for li in laundry_soup.findAll('li'):
         if 'id' in li.attrs:
-            is_washer = True if li.attrs['id'] == 'washer' else False
+            machine_type = li.attrs['id']
             continue
 
         machine_id = int(li.find('a').attrs['id'])
         machine_status = li.find('p').text
-        machine_name = li.text.split(machine_status)[0] \
-            .encode('ascii', 'ignore')
-        machine_type = 'washer' if is_washer else 'dryer'
+        machine_name = li.text.split(machine_status)[0].encode('ascii', 'ignore').decode("utf-8")
         time_left = int(machine_status[:machine_status.find(' ')]) if 'mins left' in machine_status else -1
 
         machines.append({
-            'machine_name': str(machine_name),
-            'machine_type': machine_type,
-            'machine_status': machine_status,
             'machine_id': machine_id,
+            'machine_status': machine_status,
+            'machine_name': machine_name,
+            'machine_type': machine_type,
             'time_left': time_left
         })
-
-    if machine is not None:
-        try:
-            machines = filter(lambda m: m['machine_name'] == machine, machines)[0]
-        except IndexError:
-            raise KeyError(machine + ' is not a valid machine.')
 
     return machines
