93f7c1bf8ec7c693b78398e1f52d4a7d17e8ec4d
Alex D. Zharichenko <azharichenko@gmail.com>
2018-02-18 13:49:56 -0500

    Correcting Classifier

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License, version 2',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
