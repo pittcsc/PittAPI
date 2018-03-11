72ab82423e678cfe3fc7195934cb912247a973cd
Alex D. Zharichenko <azharichenko@gmail.com>
2018-03-11 00:57:33 -0500

    Quick fix: changed .md to .rst and incremented version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
 
     description='An API to get data from Pitt and Pitt-related applications.',
     long_description=long_description,
