92e03da1aa8a16709bf99027dfd7de1d03a2a462
Alex D. Zharichenko <azharichenko@gmail.com>
2019-01-25 09:57:47 -0500

    Updated version number and added support for python 3.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',
+    version='0.2.0',
 
     description='An API to get data from Pitt and Pitt-related applications.',
     long_description=long_description,
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
