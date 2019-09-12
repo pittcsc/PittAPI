2ef3f73bb69b193eea6bcf07ccc4d7adba51077a
rohitganguly <rohitganguly88@gmail.com>
2019-09-11 23:00:18 -0400

    Added basketball info methods + comments

--- PittAPI/sports.py
+++ PittAPI/sports.py
@@ -21,46 +21,108 @@ import json
 import requests
 
 FOOTBALL_URL = 'http://site.api.espn.com/apis/site/v2/sports/football/college-football/teams/pitt'
-BASKETBALL_URL ='http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/pittsburgh'
+BASKETBALL_URL = 'http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/pittsburgh'
 
 football_response = requests.get(FOOTBALL_URL)
 basketball_response = requests.get(BASKETBALL_URL)
 
-football_data = json.loads(football_response.text)  # JSON for football football_data in an object
+football_data = json.loads(football_response.text)
 basketball_data = json.loads(basketball_response.text)
 
+
 # basketball methods
+
+# get_basketball_record() is a method designed to get the current record of the Men's basketball team via string.
+#   - During the off-season this data should return "There's no record right now".
+
+# get_next_basketball_game(input_parameter: str) is a method to get specific info on the next matchup via string.
+#   - in the off-season, this will return info on the latest game played.
+#   input_parameter specifies which type of information to get depending on what is entered.
+#   - full_name is something like "Pittsburgh Panthers vs.  Penn State Nittany Lions"
+#   - short_name is something like "PITT vs. PSU"
+#   - season_name is something like "Regular Season", "Playoffs", "Pre-season", etc.
+#   - week is something like "Week 19"
+#   - court returns the location of the game being played.
+
+# get_basketball_standings simply returns a string resembling something like "14th in ACC"
+
+
 def get_basketball_record() -> str:
     try:
         record_summary = basketball_data['team']['record']['items'][0]['summary']
 
     except KeyError:
         record_summary = "There's no record right now."
+
     return record_summary
 
 
+def get_next_basketball_game(input_parameter: str) -> str:
+
+    return_value = "Error: Input parameter"
+
+    if input_parameter == "full_name":
+        return_value = basketball_data['team']['nextEvent'][0]['name']
+    if input_parameter == "short_name":
+        return_value = basketball_data['team']['nextEvent'][0]['shortName']
+
+    if input_parameter == "season_name":
+        return_value = basketball_data['team']['nextEvent'][0]['seasonType']['name']
+
+    if input_parameter == "week":
+        return_value = basketball_data['team']['nextEvent'][0]['week']['text']
+
+    if input_parameter == "court":
+        return_value = basketball_data['team']['nextEvent'][0]['competitions'][0]['venue']['fullName']
+
+    return return_value
+
+
+def get_basketball_standings() -> str:
+    return_value = basketball_data['team']['standingSummary']
+    return return_value
+
+
+# end basketball methods
+
+
 # football methods
+# get_football_record() is a method designed to get the current record of the Men's basketball team.
+#   - During the off-season this data should return "There's no record right now" TODO: ADD THIS EXCEPTION CATCH
+
+# get_next_football_game(input_parameter: str) is a method to get specific info on the next matchup.
+#   - in the off-season, this will return info on the latest game played.
+# input_parameter specifies which type of information to get depending on what is entered.
+#   - full_name is something like "Pittsburgh Panthers vs.  Penn State Nittany Lions"
+#   - short_name is something like "PITT vs. PSU"
+#   - season_name is something like "Regular Season", "Playoffs", "Pre-season", etc.
+#   - week is something like "Week 19"
+#   - field returns the location of the game being played.
+
+# get_basketball_standings simply returns a string resembling something like "14th in ACC"
+
+
 def get_football_record() -> str:
     record_summary = football_data['team']['record']['items'][0]['summary']
     return record_summary
 
 
-def get_next_football_game(input : str) -> str:
+def get_next_football_game(input_parameter: str) -> str:
 
     return_value = "Error: Input parameter"
 
-    if input == "full_name":
+    if input_parameter == "full_name":
         return_value = football_data['team']['nextEvent'][0]['name']
-    if input == "short_name":
+    if input_parameter == "short_name":
         return_value = football_data['team']['nextEvent'][0]['shortName']
 
-    if input == "season_name":
+    if input_parameter == "season_name":
         return_value = football_data['team']['nextEvent'][0]['seasonType']['name']
 
-    if input == "week":
+    if input_parameter == "week":
         return_value = football_data['team']['nextEvent'][0]['week']['text']
 
-    if input == "field":
+    if input_parameter == "field":
         return_value = football_data['team']['nextEvent'][0]['competitions'][0]['venue']['fullName']
 
     return return_value
@@ -69,20 +131,3 @@ def get_next_football_game(input : str) -> str:
 def get_football_standings() -> str:
     return_value = football_data['team']['standingSummary']
     return return_value
-
-
-print("_______________\nBASKETBALL")
-print(get_basketball_record())
-print("_______________\nFOOTBALL")
-
-print("Record:")
-print(get_football_record())
-print("Next Game")
-print(get_next_football_game("full_name"))
-print(get_next_football_game("short_name"))
-print(get_next_football_game("season_name"))
-print(get_next_football_game("week"))
-print("Location:")
-print(get_next_football_game("field"))
-print("Standings")
-print(get_football_standings())
