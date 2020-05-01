92430625de1dbb7c693b3da4b9ab8468259cfc52
Alex D. Zharichenko <azharichenko@gmail.com>
2020-05-01 16:32:38 -0400

    Refactored lab api
    Fix lab api, because giving a lab string doesn't make much sense when it's not clear what to enter. Cleaned it up using namedtuples that just gives you all the labs in the order it's received from lab information.

--- PittAPI/lab.py
+++ PittAPI/lab.py
@@ -19,56 +19,38 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 import requests
 from bs4 import BeautifulSoup
-from typing import List, Dict
 
-session = requests.session()
+import re
+from collections import namedtuple
+from typing import List
 
-LOCATIONS = ["ALUMNI", "BENEDUM", "CATH_G27", "CATH_G62", "LAWRENCE", "HILLMAN", "SUTH"]
 URL = "http://labinformation.cssd.pitt.edu/"
 
-
-def get_status(lab_name: str) -> Dict[str, str]:
-    """Returns a dictionary with status and amount of OS machines."""
-    lab_name, labs = _validate_lab(lab_name), _fetch_labs()
-    status, *machines = labs[LOCATIONS.index(lab_name)].split(":")
-
-    if "open" in status:
-        return _make_status("open", *_extract_machines(machines[0]))
-    else:
-        return _make_status("closed")
+lab_name = re.compile(".*Lab")
+Lab = namedtuple("Lab", ["name", "status", "windows", "mac", "linux"])
 
 
 def _fetch_labs() -> List[str]:
     """Fetches text of status/machines of all labs."""
-    text = ""
-    while "Lab" not in text:
-        page = session.get(URL)
-        soup = BeautifulSoup(page.text, "lxml")
-        text = soup.span.text
-
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, "lxml")
+    text = soup.span.text
     return text.strip().split("  ")
 
 
 def _extract_machines(data: str) -> List[int]:
     """Gets available computer totals for each OS into a list."""
-    machines = []
-    for machine in data.split(","):
-        machine = machine.strip()
-        machines.append(int(machine[: machine.index(" ")]))
-    return machines
+    _, data = data.split(":")
+    return [int(i) for i in re.findall("\d+", data)]
 
 
-def _make_status(
-    state: str, win: int = 0, mac: int = 0, linux: int = 0
-) -> Dict[str, str]:
-    """Creates proper dictionary response for getting lab status."""
-    return {"status": state, "windows": str(win), "mac": str(mac), "linux": str(linux)}
-
-
-def _validate_lab(lab: str) -> str:
-    """Corrects case of lab name and checks whether it's valid."""
-    lab = lab.upper()
-    if lab in LOCATIONS:
-        return lab
-    else:
-        raise ValueError("Invalid lab name")
+def get_status() -> List[Lab]:
+    """Returns a dictionary with status and amount of OS machines."""
+    labs = []
+    for lab_data in _fetch_labs():
+        name = lab_name.search(lab_data).group()
+        status = "open" if "open" in lab_data else "closed"
+        machines = _extract_machines(lab_data) if status == "open" else [0, 0, 0]
+        computing_lab = Lab(name, status, *machines)
+        labs.append(computing_lab)
+    return labs
