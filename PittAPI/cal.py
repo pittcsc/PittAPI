bbfc11db37bf5d79f1b688682dd2286990a1dc3a
Alex D. Zharichenko <azharichenko@gmail.com>
2020-05-01 14:08:31 -0400

    Added more calendars and new parsing system

--- PittAPI/cal.py
+++ PittAPI/cal.py
@@ -21,12 +21,52 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 """
 
 import feedparser
+import re
+from typing import List
+from collections import namedtuple
 
-ACADEMIC_CALENDAR_URL = "https://25livepub.collegenet.com/calendars/pitt-academic-calendar.xml"
+content = re.compile(":&(.)*?<")
+Event = namedtuple('Event', ['timestamp', 'timestamp_struct', 'content'])
+
+ACADEMIC_CALENDAR_URL: str = "https://25livepub.collegenet.com/calendars/pitt-academic-calendar.xml"
+GRADES_CALENDAR_URL: str = "https://25livepub.collegenet.com/calendars/pitt-grades-calendar.xml"
+ENROLLMENT_CALENDAR_URL: str = "https://25livepub.collegenet.com/calendars/pitt-enrollment-calendar.xml"
+COURSE_CALENDAR_URL: str = "https://25livepub.collegenet.com/calendars/pitt-courseclass-calendar.xml"
+GRADUATION_CALENDAR_URL: str = "https://25livepub.collegenet.com/calendars/pitt-graduation-calendar.xml"
 
 
-def get_academic_calendar_events(include_summary=False):
+def _fetch_calendar_events(url: str) -> List[Event]:
     """"""
-    d = feedparser.parse(ACADEMIC_CALENDAR_URL)
-    events = [(entry.published_parsed, entry.title.upper()) for entry in d.entries]
+    d = feedparser.parse(url)
+    events = []
+    for entry in d.entries:
+        event_content = content.search(entry.summary).group()[7:-2]
+        event = Event(timestamp=entry.published, timestamp_struct=entry.published_parsed, content=event_content)
+        events.append(event)
     return events
+
+
+def get_academic_calendar() -> List[Event]:
+    """"""
+    return _fetch_calendar_events(ACADEMIC_CALENDAR_URL)
+
+
+def get_grades_calendar() -> List[Event]:
+    """"""
+    return _fetch_calendar_events(GRADES_CALENDAR_URL)
+
+
+def get_enrollment_calendar() -> List[Event]:
+    """"""
+    return _fetch_calendar_events(ENROLLMENT_CALENDAR_URL)
+
+
+def get_course_calendar() -> List[Event]:
+    """This is not a calendar about course schedule but rather
+    when courses/class are being determined for the next semester"""
+    return _fetch_calendar_events(COURSE_CALENDAR_URL)
+
+
+def get_graduation_calendar() -> List[Event]:
+    """"""
+    return _fetch_calendar_events(GRADUATION_CALENDAR_URL)
