b139fa38074761dfdee910af025ffdbf73ea6bcb
Alex D. Zharichenko <azharichenko@gmail.com>
2020-05-17 23:27:59 -0400

    Refactor bs4 out of labs

--- pittapi/lab.py
+++ pittapi/lab.py
@@ -17,40 +17,44 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-import requests
-from bs4 import BeautifulSoup
+from typing import List, NamedTuple
 
-import re
-from collections import namedtuple
-from typing import List
+from requests_html import HTMLSession
+from parse import compile
 
 URL = "http://labinformation.cssd.pitt.edu/"
 
-lab_name = re.compile(".*Lab")
-Lab = namedtuple("Lab", ["name", "status", "windows", "mac", "linux"])
+LAB_OPEN_PATTERN = compile(
+    "{name} Lab is {status}: {windows:d} Windows, {macs:d} Macs, {linux:d} Linux"
+)
+LAB_CLOSED_PATTERN = compile("{name} Lab is currently {status}")
+
+
+class Lab(NamedTuple):
+    name: str
+    status: str
+    windows: int
+    mac: int
+    linux: int
 
 
 def _fetch_labs() -> List[str]:
     """Fetches text of status/machines of all labs."""
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.text, "lxml")
-    text = soup.span.text
-    return text.strip().split("  ")
-
-
-def _extract_machines(data: str) -> List[int]:
-    """Gets available computer totals for each OS into a list."""
-    _, data = data.split(":")
-    return [int(i) for i in re.findall("\d+", data)]
+    session = HTMLSession()
+    resp = session.get(URL)
+    data = resp.html.find("#lblTextMsg", first=True)
+    return data.full_text.strip().split("  ")
 
 
 def get_status() -> List[Lab]:
     """Returns a dictionary with status and amount of OS machines."""
     labs = []
     for lab_data in _fetch_labs():
-        name = lab_name.search(lab_data).group()
-        status = "open" if "open" in lab_data else "closed"
-        machines = _extract_machines(lab_data) if status == "open" else [0, 0, 0]
-        computing_lab = Lab(name, status, *machines)
+        if "open" in lab_data:
+            content = LAB_OPEN_PATTERN.parse(lab_data)
+            computing_lab = Lab(**content)
+        else:
+            content = LAB_CLOSED_PATTERN.parse(lab_data)
+            computing_lab = Lab(**content, windows=0, mac=0, linux=0)
         labs.append(computing_lab)
     return labs
--- tests/lab_test.py
+++ tests/lab_test.py
@@ -23,7 +23,7 @@ import responses
 from pittapi import lab
 
 TEST_DATA = ('<span>Alumni Lab is currently closed.  Benedum Lab is currently closed.  Cathedral G27 Lab is currently '
-             'closed.  Cathedral G62 Lab open: 35 Windows, 15 Mac, 2 Linux  David Lawrence Lab is open: 35 Windows, 1 Mac, 2 Linux  '
+             'closed.  Cathedral G62 Lab is open: 35 Windows, 15 Mac, 2 Linux  David Lawrence Lab is open: 35 Windows, 1 Mac, 2 Linux  '
              'Hillman Lab is open: 52 Windows, 0 Macs, 2 Linux  Sutherland Lab is currently closed.</span>')
 
 
@@ -89,29 +89,3 @@ class LabTest(unittest.TestCase):
         responses.add(responses.GET, lab.URL,
                       body=TEST_DATA, status=200)
         self.assertIsInstance(lab._fetch_labs(), list)
-
-    def test_lab_name_validation(self):
-        valid, fake = lab.LOCATIONS[0].lower(), 'test'
-        self.assertTrue(lab._validate_lab(valid), lab.LOCATIONS[0])
-        self.assertRaises(ValueError, lab._validate_lab, fake)
-
-    def test_make_status(self):
-        keys = ['status', 'windows', 'mac', 'linux']
-        closed = lab._make_status('closed')
-        open = lab._make_status('open', 1, 1, 1)
-
-        self.assertIsInstance(closed, dict)
-        self.assertIsInstance(open, dict)
-
-        self.assertEqual(closed[keys[0]], 'closed')
-        self.assertEqual(open[keys[0]], 'open')
-
-        for key in keys[1:]:
-            self.assertEqual(closed[key], '0')
-            self.assertEqual(open[key], '1')
-
-    def test_extract_machines(self):
-        data = '123 hello_world, 456 macOS, 789 cool, 3 nice'
-        info = lab._extract_machines(data)
-        self.assertIsInstance(info, list)
-        self.assertEqual(info, [123, 456, 789, 3])
