3031291713e32ee99a337551363688e9fa03007a
Richie Goulazian <rgoulazian@gmail.com>
2021-01-04 14:45:16 -0500

    Make the naming scheme more readable

--- pittapi/ratemyprofessors.py
+++ pittapi/ratemyprofessors.py
@@ -19,62 +19,63 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 import requests
 from typing import Union, List, Dict, Any
 
-RESULT_FIELDS = [
-  "city_state_s",
-  "pk_id",
-  "region_i",
-  "averagehelpfulscore_rf",
-  "pageviews_i",
-  "schoolcountry_s",
-  "status_i",
-  "averagehotscore_rf",
-  "schoolstate_s",
-  "rated_date_dt",
-  "teacherfullname_s",
-  "averageteacherrating_rf",
-  "total_number_of_ratings_i",
-  "content_type_s",
-  "averagedifficultyrating_rf",
-  "averageclarityscore_rf",
-  "schoolwebpage_s",
-  "hotstatus_i",
-  "averageratingscore_rf",
-  "tag_s_mv",
-  "schoolcity_s",
-  "schoolstate_full_s",
-  "tag_id_s_mv",
-  "pict_thumb_name_s",
-  "id",
-  "timestamp",
-  "averageschoolrating_rf",
-  "geolocation",
-  "schoolname_s",
-  "teachermiddlename_t",
-  "numberofprofessors_i",
-  "schoolphoto_s",
-  "teacherdepartment_s",
-  "visible_i",
-  "averageeasyscore_rf",
-  "schoolid_s",
-  "teacherfirstname_t",
-  "teacherlastname_t",
-]
+FIELD_TO_NAME_MAPPING = {
+  "city_state": "city_state",
+  "pk_id": "professor_id",
+  "averagehelpfulscore_rf": "average_helpful_score",
+  "pageviews_i": "page_views",
+  "schoolcountry_s": "school_country",
+  "averagehotscore_rf": "average_hot_score",
+  "schoolstate_s": "school_state",
+  "rated_date_dt": "rated_date",
+  "teacherfullname_s": "teacher_full_name",
+  "total_number_of_ratings_i": "total_ratings",
+  "averagedifficultyrating_rf": "average_difficulty_rating",
+  "averageclarityscore_rf": "average_clarity_score",
+  "schoolwebpage_s": "school_webpage",
+  "averageratingscore_rf": "average_rating",
+  "tag_s_mv": "rating_tag_strings",
+  "schoolcity_s": "school_city",
+  "schoolstate_full_s": "school_state_full",
+  "tag_id_s_mv": "rating_tag_ids",
+  "pict_thumb_name_s": "picture_thumbnail_name",
+  "timestamp": "timestamp",
+  "averageschoolrating_rf": "average_school_rating",
+  "schoolname_s": "school_name",
+  "teachermiddlename_t": "teacher_middle_name",
+  "teacherdepartment_s": "teacher_department",
+  "averageeasyscore_rf": "average_easy_score",
+  "schoolid_s": "school_id",
+  "teacherfirstname_t": "teacher_first_name",
+  "teacherlastname_t": "teacher_last_name",
+}
+
+NAME_TO_FIELD_MAPPING = dict([reversed(x) for x in FIELD_TO_NAME_MAPPING.items()])
 
 DEFUALT_NUM_RESULTS = 20
 DEFAULT_RESPONSE_FIELDS = [
-  "pk_id",
-  "teacherfirstname_t",
-  "teacherlastname_t",
-  "total_number_of_ratings_i",
-  "averageratingscore_rf",
-  "averagedifficultyrating_rf"
+  "professor_id",
+  "teacher_first_name",
+  "teacher_last_name",
+  "total_ratings",
+  "average_rating_score",
+  "average_difficulty_rating"
 ]
 PITT_SCHOOL_ID = 1247
 
 RMP_SEARCH_URL = "https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/"
 
+def rename_result_fields(result: Dict[str, Any]) -> Dict[str, Any]:
+  """Rename the fields of a result from the internal RMP naming to more readable naming."""
+  renamed_result = {}
+  for result_field in result:
+    if result_field in FIELD_TO_NAME_MAPPING:
+      renamed_result[FIELD_TO_NAME_MAPPING[result_field]] = result[result_field]
+
+  return renamed_result
+
 def get_rmp_by_query(
-    query: str,additional_request_params: Dict[str, str]={}) -> List[Dict[str, Any]]:
+    query: str, additional_request_params: Dict[str, str]={}) -> List[Dict[str, Any]]:
   """Return the results of a custom Apache Solr query to RMP."""
   request_params = {
     "wt": "json",
@@ -82,7 +83,11 @@ def get_rmp_by_query(
     **additional_request_params
   }
 
-  return requests.get(RMP_SEARCH_URL, params=request_params).json()['response']['docs']
+  results = requests.get(RMP_SEARCH_URL, params=request_params).json()['response']['docs']
+  for x in range(len(results)):
+    results[x] = rename_result_fields(results[x])
+  
+  return results
 
 def get_rmp_by_name(
   prof_name: str,
@@ -103,7 +108,7 @@ def get_rmp_by_name(
       "qf": "teacherfirstname_t^2000 teacherlastname_t^2000 teacherfullname_t^2000 autosuggest", # https://lucene.apache.org/solr/guide/6_6/the-dismax-query-parser.html#TheDisMaxQueryParser-Theqf_QueryFields_Parameter
       "bf": "pow(total_number_of_ratings_i,2.1)", # https://lucene.apache.org/solr/guide/6_6/the-dismax-query-parser.html#TheDisMaxQueryParser-Thebf_BoostFunctions_Parameter
       "sort": "total_number_of_ratings_i desc", # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-ThesortParameter
-      "fl": " ".join(response_fields), # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-Thefl_FieldList_Parameter
+      "fl": " ".join([NAME_TO_FIELD_MAPPING[x] for x in response_fields]), # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-Thefl_FieldList_Parameter
       "rows": num_results,
     }
   )
@@ -131,7 +136,7 @@ def get_rmp_by_name_fuzzy(
       "qf": "teacherfirstname_t^2000 teacherlastname_t^2000 teacherfullname_t^2000 autosuggest", # https://lucene.apache.org/solr/guide/6_6/the-dismax-query-parser.html#TheDisMaxQueryParser-Theqf_QueryFields_Parameter
       "bf": "pow(total_number_of_ratings_i,2.1)", # https://lucene.apache.org/solr/guide/6_6/the-dismax-query-parser.html#TheDisMaxQueryParser-Thebf_BoostFunctions_Parameter
       "sort": "total_number_of_ratings_i desc", # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-ThesortParameter
-      "fl": " ".join(response_fields), # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-Thefl_FieldList_Parameter
+      "fl": " ".join([NAME_TO_FIELD_MAPPING[x] for x in response_fields]), # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-Thefl_FieldList_Parameter
       "rows": num_results,
     }
   )
@@ -145,7 +150,7 @@ def get_rmp_by_id(
   query_results = get_rmp_by_query(
     query="pk_id:{prof_id}".format(prof_id=prof_id),
     additional_request_params={
-      "fl": " ".join(response_fields), # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-Thefl_FieldList_Parameter,
+      "fl": " ".join([NAME_TO_FIELD_MAPPING[x] for x in response_fields]), # https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html#CommonQueryParameters-Thefl_FieldList_Parameter,
     }
   )
 
